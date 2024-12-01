2ffa63cbdbead60034e443240af405e675bb14ff
nguyeenphan <phannguyenbh0902@gmail.com>
2024-12-01 14:20:34 +0700

    Update Vite config for build output and chunk size

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Ensure this is set to 'build'
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the chunk size limit as needed
+  },
 })
