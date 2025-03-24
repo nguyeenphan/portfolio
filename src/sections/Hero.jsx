f5d0c2b6454297b48a9914c80531f813f1c3979f
nguyeenphan <phannguyenbh0902@gmail.com>
2025-03-24 14:18:08 +0700

    view cv

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,8 @@ import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera } from "@react-three/drei";
 import { Suspense } from "react";
 import CanvasLoader from "../components/CanvasLoader.jsx";
-import {useMediaQuery} from "react-responsive";
-import {calculateSizes} from "../constants/index.js";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
 import Target from "../components/Target.jsx";
 import ReactLogo from "../components/ReactLogo.jsx";
 import Cube from "../components/Cube.jsx";
@@ -13,47 +13,51 @@ import Button from "../components/Button.jsx";
 import Candy from "../components/Candy.jsx";
 
 function Hero() {
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ maxWidth: 768, maxHeight: 1024 });
 
-    const isSmall = useMediaQuery({maxWidth: 440});
-    const isMobile = useMediaQuery({maxWidth: 768});
-    const isTablet = useMediaQuery({maxWidth: 768, maxHeight: 1024});
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
-    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+  return (
+    <section className="min-h-screen w-full flex flex-col relative" id="home">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hi, I am Nguyen Phan <span className="waving-hand">🖐🏼️</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Software Engineer & UX Designer
+        </p>
+      </div>
 
-    return (
-        <section className="min-h-screen w-full flex flex-col relative" id='home'>
-            <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
-                    Hi, I am Nguyen Phan <span className="waving-hand">🖐🏼️</span>
-                </p>
-                <p className="hero_tag text-gray_gradient">Software Engineer & UX Designer</p>
-            </div>
-
-            <div className="w-full h-full absolute inset-0">
-                <Canvas className="w-full h-full">
-                    <Suspense fallback={<CanvasLoader />}>
-                        <PerspectiveCamera makeDefault position={[0, 0, 20]} />
-                        <HeroCamera isMobile={isMobile}>
-                            <Target position={sizes.deskPosition} scale={sizes.deskScale}/>
-                        </HeroCamera>
-                        <group>
-                            <Candy position={sizes.targetPosition}/>
-                            <ReactLogo position={sizes.reactLogoPosition}/>
-                            <Cube position={sizes.cubePosition}/>
-                            <Rings position={sizes.ringPosition}/>
-                        </group>
-                        <ambientLight intensity={0.5} />
-                        <directionalLight position={[10, 10, 10]} intensity={1} />
-                    </Suspense>
-                </Canvas>
-            </div>
-            <div className='absolute bottom-7 left-0 right-0 z-10 c-space'>
-                <a href='#about' className='w-fit'>
-                    <Button name="Let's Work Together" isBeam containerClass='sm:w-fit w-full sm:min-w-96'/>
-                </a>
-            </div>
-        </section>
-    );
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HeroCamera isMobile={isMobile}>
+              <Target position={sizes.deskPosition} scale={sizes.deskScale} />
+            </HeroCamera>
+            <group>
+              <Candy position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Cube position={sizes.cubePosition} />
+              <Rings position={sizes.ringPosition} />
+            </group>
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[10, 10, 10]} intensity={1} />
+          </Suspense>
+        </Canvas>
+      </div>
+      <div className="absolute bottom-7 left-0 right-0 z-10 c-space">
+        <a href="#about" onClick={() => window.open("https://drive.google.com/file/d/1QJt_56TQKwYCkt845aRi1gwqnNtG2KAm/view?usp=sharing", '_blank')} className="w-fit">
+          <Button
+            name="View My Resume"
+            containerClass="sm:w-fit w-full sm:min-w-96"
+          />
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
